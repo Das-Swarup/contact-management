ba90e1839718e01681c475ba821ad8635062ec16
Das-Swarup <swarupd049@gmail.com>
2023-06-03 16:53:57 +0530

    modified styling and server route

--- src/Redux/Add_Contacts/addContacts.action.js
+++ src/Redux/Add_Contacts/addContacts.action.js
@@ -5,7 +5,7 @@ import { ADD_CONTACT_ERROR, ADD_CONTACT_SUCCESS } from "./addContacts.actiontype
 
 export const addContact=(contact)=>async(dispatch)=>{
   try {
-    let res=await axios.post("https://taiyo-ai-server.onrender.com/contacts",contact)
+    let res=await axios.post("https://my-json-server.typicode.com/Das-Swarup/contact-management/contacts",contact)
     dispatch({ type: ADD_CONTACT_SUCCESS, payload:res.data });
   } catch (error) {
     dispatch({ type: ADD_CONTACT_ERROR, payload: error.message });
--- src/components/ChartsAndMaps.tsx
+++ src/components/ChartsAndMaps.tsx
@@ -13,14 +13,14 @@ const fetchChartData = async () => {
 const ChartsAndMaps = () => {
   const [chartData, setChartData] = useState({});
   const [countryData, setCountryData] = useState([]);
-  const [windwindth,setWindwidth]=useState({wid:window.innerWidth})
- 
+  const [windwindth, setWindwidth] = useState({ wid: window.innerWidth })
 
 
-const fetchCountryData = async () => {
+
+  const fetchCountryData = async () => {
     const res = await axios.get('https://disease.sh/v3/covid-19/countries');
     const data = res.data;
-  
+
     const countryData = data.map((country) => ({
       name: country.country,
       lat: country.countryInfo.lat,
@@ -29,74 +29,74 @@ const fetchCountryData = async () => {
       recovered: country.recovered,
       deaths: country.deaths,
     }));
-  
+
     setCountryData(countryData);
   };
-useEffect(()=>{
-  fetchChartData().then((res)=>setChartData({
-    labels:Object.keys(res.data.cases),
-    datasets:[
-      {
-        label:"COVID-19 Cases",
-        data:Object.values(res.data.cases),
-        backgroundColor:"red",
-      }
-    ]
-  }))
-  fetchCountryData()
-},[])
+  useEffect(() => {
+    fetchChartData().then((res) => setChartData({
+      labels: Object.keys(res.data.cases),
+      datasets: [
+        {
+          label: "COVID-19 Cases",
+          data: Object.values(res.data.cases),
+          backgroundColor: "red",
+        }
+      ]
+    }))
+    fetchCountryData()
+  }, [])
 
 
-useEffect(() => {
-  const chartConfig = {
-    type: 'line',
-    data: chartData,
-  };
+  useEffect(() => {
+    const chartConfig = {
+      type: 'line',
+      data: chartData,
+    };
 
-  const myChart = new Chart(document.getElementById('myChart'), chartConfig);
-  return () => {
-    myChart.destroy();
-  };
-}, [chartData]);
+    const myChart = new Chart(document.getElementById('myChart'), chartConfig);
+    return () => {
+      myChart.destroy();
+    };
+  }, [chartData]);
 
   return (
     <div>
-        <Heading color={'white'} p={"10px 20px"} bg={"#28686e"}>Charts and Maps</Heading>
-        <div id="charts_page_div">
-        {window.innerWidth>900?<Box padding={"10px"} w={"19%"} boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"}>
-               <Box><Link style={{textDecoration:"none",fontSize:"20px",fontWeight:"bold"}} to="/">Contacts</Link></Box>
-               <br />
-               <br />
-               <Box><Link style={{textDecoration:"none",fontSize:"20px",fontWeight:"bold"}} to="/chartsandmaps">Charts & Maps</Link></Box>
-            </Box>:<Flex justifyContent={"space-evenly"} w={"100%"} margin={'auto'} marginBottom={"20px"} p={"10px 0px"}  boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"}>
-               <Box><Link style={{textDecoration:"none",fontSize:"20px",fontWeight:"bold"}} to="/">Contacts</Link></Box>
-               <Box><Link style={{textDecoration:"none",fontSize:"20px",fontWeight:"bold"}} to="/chartsandmaps">Charts & Maps</Link></Box>
-            </Flex>}
-            <Box padding={"30px"} margin={'auto'}  w={"79%"} boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"} border={"1px solid gray"}>
-            <Heading>COVID-19 Dashboard</Heading>
-             <Box >
-                {window.innerWidth>900?<canvas id="myChart" width="800" height="300"></canvas>:<canvas id="myChart" width="400" height="300"></canvas>}
-             </Box>
-             <br />
-             <br />
-             <Box>
-                <MapContainer center={[0, 0]} zoom={2}  >
-                   <TileLayer url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=NLe8DG6CVIhkI4PpAXR1" />
-                   {countryData.map((country) => (
-                       <Marker key={country.name} position={[country.lat, country.long]}>
-                          <Popup>
-                            <h4>Name: {country.name}</h4>
-                            <p>Active Cases: {country.active}</p>
-                            <p>Recovered Cases: {country.recovered}</p>
-                            <p>Deaths: {country.deaths}</p>
-                          </Popup>
-                        </Marker>
-                    ))}
-                </MapContainer>
-             </Box>
+      <Heading color={'white'} p={"10px 20px"} bg={"#28686e"}>Charts and Maps</Heading>
+      <div id="charts_page_div">
+        {window.innerWidth > 900 ? <Box padding={"10px"} w={"19%"} boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"}>
+          <Box><Link style={{ textDecoration: "none", fontSize: "20px", fontWeight: "bold" }} to="/">Contacts</Link></Box>
+          <br />
+          <br />
+          <Box><Link style={{ textDecoration: "none", fontSize: "20px", fontWeight: "bold" }} to="/chartsandmaps">Charts & Maps</Link></Box>
+        </Box> : <Flex justifyContent={"space-evenly"} w={"100%"} margin={'auto'} marginBottom={"20px"} p={"10px 0px"} boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"}>
+          <Box><Link style={{ textDecoration: "none", fontSize: "20px", fontWeight: "bold" }} to="/">Contacts</Link></Box>
+          <Box><Link style={{ textDecoration: "none", fontSize: "20px", fontWeight: "bold" }} to="/chartsandmaps">Charts & Maps</Link></Box>
+        </Flex>}
+        <Box padding={"30px"} margin={'auto'} w={"79%"} boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"} border={"1px solid gray"}>
+          <Heading color={"black"}>COVID-19 Dashboard</Heading>
+          <Box >
+            {window.innerWidth > 900 ? <canvas id="myChart" width="800" height="300"></canvas> : <canvas id="myChart" width="400" height="300"></canvas>}
           </Box>
+          <br />
+          <br />
+          <Box>
+            <MapContainer center={[0, 0]} zoom={2}  >
+              <TileLayer url="https://api.maptiler.com/maps/basic-v2/256/{z}/{x}/{y}.png?key=NLe8DG6CVIhkI4PpAXR1" />
+              {countryData.map((country) => (
+                <Marker key={country.name} position={[country.lat, country.long]}>
+                  <Popup>
+                    <h4>Name: {country.name}</h4>
+                    <p>Active Cases: {country.active}</p>
+                    <p>Recovered Cases: {country.recovered}</p>
+                    <p>Deaths: {country.deaths}</p>
+                  </Popup>
+                </Marker>
+              ))}
+            </MapContainer>
+          </Box>
+        </Box>
 
-        </div>
+      </div>
     </div>
   )
 }
